d995d9da96c0bf8f748eb0413d08f55f1133f418
Alison Williams <apw5011@gmail.com>
2020-11-11 01:40:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 def prime?(int)
   if int <= 1 
     return false 
-  elsif (2..int).to_a.each { |n| int % n == 0 && n < int }
+  elsif (2..int).each { |n| int % n == 0 && n < int }
     return false 
   else 
     return true 
